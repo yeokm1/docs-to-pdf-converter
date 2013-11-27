24d6f8483ac5460552dc51767132ffb2882b3f02
Yeo Kheng Meng <yeokm1@yahoo.com.sg>
2013-11-28 01:28:38 +0800

    Disable stdout when convert doc to docx

--- docs-to-pdf-converter/src/DocToPDFConverter.java
+++ docs-to-pdf-converter/src/DocToPDFConverter.java
@@ -1,4 +1,6 @@
 import java.io.FileInputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
 
 import org.docx4j.convert.in.Doc;
 import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
@@ -12,7 +14,17 @@ public class DocToPDFConverter extends DocxToPDFConverter {
 
 	@Override
 	protected WordprocessingMLPackage getMLPackage(FileInputStream iStream) throws Exception{
-	    WordprocessingMLPackage mlPackage = Doc.convert(iStream);
+		PrintStream originalStdout = System.out;
+		
+		//Disable stdout temporarily as Doc convert produces alot of output
+		System.setOut(new PrintStream(new OutputStream() {
+			public void write(int b) {
+				//DO NOTHING
+			}
+		}));
+		WordprocessingMLPackage mlPackage = Doc.convert(iStream);
+		
+		System.setOut(originalStdout);
 		return mlPackage;
 	}
 	
