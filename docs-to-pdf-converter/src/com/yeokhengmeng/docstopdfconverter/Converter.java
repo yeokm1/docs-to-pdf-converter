37235d96401a3191ad2c67903feabf0c975eef74
Yeo Kheng Meng <yeokm1@yahoo.com.sg>
2013-12-01 21:24:50 +0800

    Removed legacy variables

--- docs-to-pdf-converter/src/com/yeokhengmeng/docstopdfconverter/Converter.java
+++ docs-to-pdf-converter/src/com/yeokhengmeng/docstopdfconverter/Converter.java
@@ -7,70 +7,56 @@ import java.io.OutputStream;
 
 public abstract class Converter {
 
-	
-	private final String LOADING_FORMAT = "\nLoading file \"%1$s\"\n\n";
+
+	private final String LOADING_FORMAT = "\nLoading stream\n\n";
 	private final String PROCESSING_FORMAT = "Load completed in %1$dms, now converting...\n\n";
-	private final String SAVING_FORMAT = "Conversion to \"%1$s\" took %2$dms.\n\nTotal: %3$dms\n";
-	
+	private final String SAVING_FORMAT = "Conversion took %1$dms.\n\nTotal: %2$dms\n";
+
 	private long startTime;
 	private long startOfProcessTime;
-	
-	protected String inputFilePath;
-	protected String outputFilePath;
-	
+
 	protected InputStream inStream;
 	protected OutputStream outStream;
-	
+
 	protected boolean showOutputMessages = false;
 	protected boolean closeStreamsWhenComplete = true;
-	
+
 	public Converter(InputStream inStream, OutputStream outStream, boolean showMessages, boolean closeStreamsWhenComplete){
 		this.inStream = inStream;
 		this.outStream = outStream;
 		this.showOutputMessages = showMessages;
 		this.closeStreamsWhenComplete = closeStreamsWhenComplete;
 	}
-	
-//	public Converter(String inputFilePath, String outputFilePath, boolean showMessages, boolean closeStreamsWhenComplete) throws IOException {
-//		inStream = getInFileStream(inputFilePath);
-//		outStream = getOutFileStream(outputFilePath);	
-//		this.showOutputMessages = showMessages;
-//		
-//		inputFilePath = inStream.toString();
-//		outputFilePath = outStream.toString();
-//		this.closeStreamsWhenComplete = closeStreamsWhenComplete;
-//	}
-	
-	
+
 	public abstract void convert() throws Exception;
-	
+
 	private void startTime(){
 		startTime = System.currentTimeMillis();
 		startOfProcessTime = startTime;
 	}
-	
+
 	protected void loading(){
-		printMessages(String.format(LOADING_FORMAT, inputFilePath));
+		printMessages(String.format(LOADING_FORMAT));
 		startTime();
 	}
-	
+
 	protected void processing(){
 		long currentTime = System.currentTimeMillis();
 		long prevProcessTook = currentTime - startOfProcessTime;
-		
+
 		printMessages(String.format(PROCESSING_FORMAT, prevProcessTook));
-		
+
 		startOfProcessTime = System.currentTimeMillis();
-		
+
 	}
-	
+
 	protected void finished(){
 		long currentTime = System.currentTimeMillis();
 		long timeTaken = currentTime - startTime;
 		long prevProcessTook = currentTime - startOfProcessTime;
 
 		startOfProcessTime = System.currentTimeMillis();
-		
+
 		if(closeStreamsWhenComplete){
 			try {
 				inStream.close();
@@ -79,22 +65,22 @@ public abstract class Converter {
 				//Nothing done
 			}
 		}
-		
-		printMessages(String.format(SAVING_FORMAT, outputFilePath, prevProcessTook, timeTaken));
+
+		printMessages(String.format(SAVING_FORMAT, prevProcessTook, timeTaken));
 	}
-	
-	
+
+
 	protected void printMessages(String toBePrinted){
 		if(showOutputMessages){
 			System.out.println(toBePrinted);
 		}
 	}
-	
-	
-	
-	
-	
-	
-	
-	
+
+
+
+
+
+
+
+
 }
